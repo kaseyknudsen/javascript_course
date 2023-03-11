66aa2b3479bf82298ad18c4f26373ec796645172
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-11 08:15:38 -0800

    more functions

---
+++ functions/function-challenges.js
@@ -0,0 +1,6 @@
+//challenge 1
+/* create a function that takes a temperature in fahrenheit as an
+argument and returns the temp in celsius */
+
+const getCelsius = (temp) => (temp - 32) * .5556
+console.log(getCelsius(32)) 
