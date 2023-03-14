0898c5d2a26ba7fbf46358c7fa477baa898f8994
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-14 15:45:07 -0700

    callstack notes

---
+++ functions/callStack.js
@@ -0,0 +1,12 @@
+/*  
+//a stack is a data structure
+The Call Stack
+stack of functions to be executed
+manages execution contexts
+stacks are LIFO (last in first out)
+*/
+
+//third()
+//second()
+//first()
+//Global EC
