d90b6fb86a3f0965709ad7f2b0408bef7de5228b
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-03-19 09:13:28 -0700

    break and continue

---
+++ loops/breakAndContinue.js
@@ -0,0 +1,17 @@
+//break will break out of the entire loop
+for (let i = 0; i <= 20; i++) {
+  if (i === 15) {
+    console.log("breaking");
+    break;
+  }
+  console.log(`i: ${i}`)
+}
+
+//continue will skip the expression in the conditional expression and continue on
+for (let i = 0; i <= 20; i++) {
+    if (i === 13) {
+        console.log('skipping 13')
+        continue
+    }
+    console.log(i)
+}
