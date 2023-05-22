8b57f5faa7e28e88b7aa2d37db41ff554800ec49
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-22 14:23:54 -0700

    DOM selector multiple elements

--- DOM/shoppingList/shoppingList.js
+++ DOM/shoppingList/shoppingList.js
@@ -10,34 +10,35 @@ output = document.domain;
 output = document.URL;
 output = document.images[0];
 output = document.images[0].src;
-console.log(output);
-console.log(document.getElementById("app-title"));
+// console.log(output);
+// console.log(document.getElementById("app-title"));
 /* you can create an array from an html collection like this: */
 forms = Array.from(document.forms);
-console.log(forms);
+// console.log(forms);
 
-forms.forEach((form) => console.log(form));
+forms.forEach((form) => console.log("forms" + form));
 
 //document.getElementById()
-console.log(
-  ` get attribute: ${document.getElementById("app-title").getAttribute("id")}`
-);
+// console.log(
+//   ` get attribute: ${document.getElementById("app-title").getAttribute("id")}`
+// );
 
 //set attributes
 // document.getElementById('app-title').id = 'new-id' //this sets a new id
-document.getElementById("app-title").title = "Shopping-List"; //this will pop up when you hover over the title "Shopping List"
-document.getElementById("app-title").setAttribute = ("class", "title"); //this will pop up when you hover over the title "Shopping List"
+const titleVariable = (document.getElementById("app-title").title =
+  "Shopping-List"); //this will pop up when you hover over the title "Shopping List"
+titleVariable.setAttribute = ("class", "title"); //this will pop up when you hover over the title "Shopping List"
 
 const title = document.getElementById("app-title");
-console.log(`title text content: ${title.textContent}`);
+// console.log(`title text content: ${title.textContent}`);
 
 //get/change content
 title.textContent = "Hello eric";
-console.log(`title text content: ${title.textContent}`);
+// console.log(`title text content: ${title.textContent}`);
 title.innerText = "Hello kasey";
-console.log(`title text content: ${title.innerText}`);
+// console.log(`title text content: ${title.innerText}`);
 title.innerHTML = "<strong>Shopping List</strong>";
-console.log(`title inner HTML: ${title.innerHTML}`);
+// console.log(`title inner HTML: ${title.innerHTML}`);
 
 //change styles
 title.style.color = "green";
@@ -52,18 +53,43 @@ const h1 = document.querySelector("h1");
 const selectById = document.querySelector("#app-title");
 const selectByClass = document.querySelector(".container");
 const selectByInput = document.querySelector('input[type="text"]');
-console.log(h1);
-console.log(selectById);
-console.log(selectByClass);
-console.log(`select by input: ${selectByInput}`);
+// console.log(h1);
+// console.log(selectById);
+// console.log(selectByClass);
+// console.log(`select by input: ${selectByInput}`);
 const orangeJuice = document.querySelector("li:nth-child(2)").innerText;
-console.log(`use CSS pseudo selector: ${orangeJuice}`);
+// console.log(`use CSS pseudo selector: ${orangeJuice}`);
 const secondItem = document.querySelector("li:nth-child(2)");
 secondItem.innerText = "Bagels";
-console.log(secondItem);
+// console.log(secondItem);
 secondItem.style.color = "blue";
 
 //use these methods on other elements other than the document
-const list = document.querySelector('ul')
-const firstItem = list.querySelector('li')
-firstItem.style.color = "red"
+const list = document.querySelector("ul");
+const firstItem = list.querySelector("li");
+firstItem.style.color = "red";
+
+//querySelectorAll()
+
+const listItems = document.querySelectorAll(".items");
+console.log(listItems);
+
+const dogs = document.querySelectorAll("#dog-menu option");
+
+dogs.forEach((dog, index) => {
+  if (index === 1) {
+    dog.remove();
+  }
+  if (index === 2) {
+    dog.innerText = "Junior";
+  }
+});
+
+const listItems2 = document.querySelectorAll(".items");
+listItems2.forEach((item) => {
+  console.log(item.innerText)
+});
+
+const listItems3 = document.getElementsByClassName(".items")
+const listItems3Array = Array.from(listItems3)
+// listItems3Array.forEach((item) => console.log(`item: ${item}`))
