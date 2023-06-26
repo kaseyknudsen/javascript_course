b15fbb03fd302d42eba2ab15f4e542d273c34225
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-06-26 12:44:17 -0700

    centered boxes on cross and main axis

--- keyCode-project/index.js
+++ keyCode-project/index.js
@@ -1,21 +1,22 @@
-const createBox = (className, textToAdd) => {
+const newBox = (className, textToAdd) => {
   const div = document.createElement("div");
   div.className = className;
   const text = document.createTextNode(textToAdd);
   div.appendChild(text);
-  document.body.appendChild(div);
+  return div;
 };
+
+let numBoxes = 5;
 
 const myContainer = () => {
   const container = document.createElement("div");
   container.className = "container";
+  for (i = 0; i < numBoxes; i++) {
+    const box = newBox("box", "I am a box");
+    container.appendChild(box);
+  }
+
   document.body.appendChild(container);
-  const box = createBox("box", "I am a box");
 };
 
-let numBoxes = 3;
-
-const createBoxes = () => {};
-for (i = 0; i < numBoxes; i++) {
-  createBox("box", "I am a box");
-}
+myContainer();
