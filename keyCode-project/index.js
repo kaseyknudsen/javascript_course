2bf3e0c289476d3049338aa23ebcb4a64ecd7277
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-06-27 13:05:53 -0700

    key codes project complete

--- keyCode-project/index.js
+++ keyCode-project/index.js
@@ -1,23 +1,43 @@
-// const newBox = (className, textToAdd) => {
-//   const div = document.createElement("div");
-//   div.className = className;
-//   const text = document.createTextNode(textToAdd);
-//   div.appendChild(text);
-//   return div;
-// };
+//method 1
+// window.addEventListener("keydown", (e) => {
+//   const insert = document.querySelector("#insert");
+//   insert.innerHTML = ` <div class="key">
+//   ${e.key === " " ? "Space Bar" : e.key}
+//   <small>e.key</small>
+// </div>
 
-// let numBoxes = 5;
-// const textList = ["Box 1", "Box 2", "Box 3"];
+// <div class="key">
+//   ${e.keyCode}
+//   <small>e.keyCode</small>
+// </div>
 
-// const myContainer = () => {
-//   const container = document.createElement("div");
-//   container.className = "container";
-//   textList.map((boxNum, idx) => {
-//     const box = newBox("box", boxNum);
-//     container.appendChild(box);
-//   });
+// <div class="key">
+//   ${e.code}
+//   <small>e.code</small>
+// </div>`;
+// });
 
-//   document.body.appendChild(container);
-// };
+//method 2 
 
-// myContainer();
+const showKeyCodes = (e) => {
+  const insert = document.querySelector("#insert");
+  insert.innerHTML = " ";
+  const keyCodes = {
+    "e.key": e.key === " " ? "Space Bar" : e.key,
+    "e.keyCode": e.keyCode,
+    "e.code": e.code,
+  };
+  for (let key in keyCodes) {
+    const div = document.createElement("div");
+    div.className = "key";
+    const small = document.createElement("small");
+    const keyText = document.createTextNode(key);
+    //this is the syntax for getting the value of an object
+    const valueText = document.createTextNode(keyCodes[key]);
+    small.appendChild(keyText);
+    div.appendChild(valueText);
+    div.appendChild(small);
+    insert.appendChild(div);
+  }
+};
+window.addEventListener("keydown", showKeyCodes);
