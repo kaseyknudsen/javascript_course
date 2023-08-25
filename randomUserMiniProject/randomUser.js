bb0d87a09e8a6b9eaea73a283aa4fd6257f0ad6b
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-24 21:13:43 -0700

    finished first part of random user project

--- randomUserMiniProject/randomUser.js
+++ randomUserMiniProject/randomUser.js
@@ -7,12 +7,16 @@ const gender = document.createElement("h3");
 const userLocation = document.createElement("h3");
 const phone = document.createElement("h3");
 const email = document.createElement("h3");
+const image = document.createElement("div");
+image.className = "imageClass";
+userProfile.appendChild(image);
 userProfile.appendChild(userName);
 userProfile.appendChild(age);
 userProfile.appendChild(gender);
 userProfile.appendChild(userLocation);
 userProfile.appendChild(phone);
 userProfile.appendChild(email);
+const button = document.getElementById("generateUser");
 
 const fetchUser = () => {
   fetch("https://randomuser.me/api")
@@ -29,12 +33,12 @@ const displayUser = (user) => {
   } else {
     document.body.style.backgroundColor = "steel blue";
   }
-  userName.textContent = `Name: ${user.name.title} ${user.name.first} ${user.name.last}`
-  gender.textContent = `${user.gender}`
-  age.textContent = `Age: ${user.age}`
-  userLocation.textContent = `Location: ${user.location.city} ${user.location.state}`
-  email.textContent = `Email: ${user.email}`
- 
+  image.innerHTML = `<img class="w-48 h-48 rounded" src="${user.picture.large}" />`;
+  userName.textContent = `Name: ${user.name.title} ${user.name.first} ${user.name.last}`;
+  gender.textContent = `${user.gender}`;
+  age.textContent = `Age: ${user.dob.age}`;
+  userLocation.textContent = `Location: ${user.location.city} ${user.location.state}`;
+  email.textContent = `Email: ${user.email}`;
 };
 
-fetchUser();
+button.addEventListener("click", fetchUser);
